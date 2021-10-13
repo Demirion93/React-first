07118cf337f6b431424d5e4797f1cf18dc03499c
Szymon Wierucki <szymon.wierucki@gmail.com>
2021-10-14 00:48:09 +0200

    Add code for task 16.4

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,16 +1,22 @@
 import {connect} from 'react-redux';
+import {getCardsForColumn, createActionAddCard} from '../../redux/cardsRedux.js';
 import Column from './Column';
 
-export const getCardsForColumn = ({cards}, columnId) => {
-  console.log(cards, columnId);
-  return cards.filter(card => card.columnId == columnId);
+// export const getCardsForColumn = ({cards}, columnId) => {
+//   console.log(cards, columnId);
+//   return cards.filter(card => card.columnId == columnId);
+// };
 
-};
-
+const mapDispatchToProps = (dispatch, props) => ({
+  addCard: title => dispatch(createActionAddCard({
+    columnId: props.id,
+    title,
+  })),
+});
 
 
 const mapStateToProps = (state, props) => ({
   cards: getCardsForColumn(state, props.id),
 });
 
-export default connect(mapStateToProps)(Column);
+export default connect(mapStateToProps, mapDispatchToProps)(Column);
