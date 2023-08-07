1a403b4a9f0a6c547f7940f9db52a3e0da643c7a
Szymon Wierucki <szymon.wierucki@gmail.com>
2023-08-07 16:20:26 +0200

    Change ReactDom.render

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { HashRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><Provider store={store}><App /></Provider></Router>, document.getElementById('app'));
 
